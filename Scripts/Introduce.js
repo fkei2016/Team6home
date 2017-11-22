8cbe1e5383779d5df7c28dbb6f69a0f32130ec49
TSITCL\s152132 <shinnjitaguchi@gmail.com>
2017-11-22 11:49:20 +0900

    リンク

--- Scripts/Introduce.js
+++ Scripts/Introduce.js
@@ -3,29 +3,32 @@ document.addEventListener('DOMContentLoaded',
     function () {
 
 
+        document.write('<a href="../HTML/F_Job.html"><img src="../Images/Trident.png"></a>');
+
+
+
+
         
 
 
 
 
 
+        //document.write('<img src="../Images/Trident.png"/>'); //  width="104" height="91" 
 
 
 
+        //var anchor = document.createElement("a")
+        //anchor.href = "http://computer.trident.ac.jp/";
 
+        //var text = document.createTextNode(anchor);
+        //anchor.appendChild(text);
 
+        //var anchor2 = document.createElement("hr")
 
-        var anchor = document.createElement("a")
-        anchor.href = "http://computer.trident.ac.jp/";
-
-        var text = document.createTextNode(anchor);
-        anchor.appendChild(text);
-
-        var anchor2 = document.createElement("hr")
-
-        var list = document.getElementById("id_div_introduce");
-        list.appendChild(anchor);
-        list.appendChild(anchor2);
+        //var list = document.getElementById("id_div_introduce");
+        //list.appendChild(anchor);
+        //list.appendChild(anchor2);
 
     }, false);
 /*-----------------------------------------------------------------*/
