8a8c7121de34320bc8c301542c559377c6644d55
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-11-27 10:42:31 +0900

    Header Nav

--- Scripts/Top.js
+++ Scripts/Top.js
@@ -6,4 +6,17 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = '../HTML/Top.html';
     };
 
+    var introButton = document.getElementById("intro");
+    introButton.onclick = function () {
+        window.location.href = '../HTML/Introduce.html';
+    }
+    var jobButton = document.getElementById("job");
+    jobButton.onclick = function () {
+        window.location.href = '../HTML/F_Job.html';
+    }
+    var testButton = document.getElementById("test");
+    testButton.onclick = function () {
+        window.location.href = '../HTML/test.html';
+    }
+
 }, false);
