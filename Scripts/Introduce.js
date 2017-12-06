e1348457c77606762e1e7324e5472de0f0927e3b
TSITCL\s152132 <shinnjitaguchi@gmail.com>
2017-12-06 11:15:52 +0900

    1206_幅の設定

--- Scripts/Introduce.js
+++ Scripts/Introduce.js
@@ -1,95 +1,13 @@
-﻿/*-----------------------------------------------------------------*/
+﻿$(function () {
+    hoge();
+});
 
+/*-----------------------------------------------------------------*/
+function hoge() {
 
-document.addEventListener('DOMContentLoaded',
-    function () {
+    
+    document.write("hoge");
 
-        //document.write('<img src="../Images/GameScienceClass.jpg" alt="画像だよ♪" align="left">');
-
-
-
-        //var reader = new FileReader();
-
-
-
-
-
-
-
-
-        //document.getElementById("file").onchange = function(e){
-        //    reader.addEventListener('load', function(e) {
-        //        document.getElementById("img").setAttribute("src",e.target.result);
-        //    });
-        //    reader.readAsDataURL( this.files[0] );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //var input = "../Images/GameScienceClass.jpg";
-
-
-        //var reader = new FileReader();
-        //reader.addEventListener('load', function () {
-        //    document.getElementById("result").src = reader.result;
-        //}, true);
-        //reader.readAsDataURL(input);
-
-
-        //document.write('<img src="../Images/GameScienceClass.jpg">');
-
-
-
-
-        // GT1の紹介
-
-
-
-
-
-
-
-
-
-
-
-
-        //document.write('<a href="../HTML/F_Job.html"><img src="../Images/Trident.png"></a>');
-
-
-
-
-        
-
-
-
-
-
-        //document.write('<img src="../Images/Trident.png"/>'); //  width="104" height="91" 
-
-
-
-        //var anchor = document.createElement("a")
-        //anchor.href = "http://computer.trident.ac.jp/";
-
-        //var text = document.createTextNode(anchor);
-        //anchor.appendChild(text);
-
-        //var anchor2 = document.createElement("hr")
-
-        //var list = document.getElementById("id_div_introduce");
-        //list.appendChild(anchor);
-        //list.appendChild(anchor2);
-
-    }, false);
+    return "hoge";
+}
 /*-----------------------------------------------------------------*/
