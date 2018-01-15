75a411795265d143c5fc77294e4aba9be01d8138
Fujii <fkei120@yahoo.co.jp>
2018-01-15 10:05:27 +0900

    多分完成！！

--- Scripts/F_Job.js
+++ Scripts/F_Job.js
@@ -21,52 +21,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var aquriaButton = document.getElementById("aquria");
     aquriaButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'http://www.aquria.co.jp';
     }
 
     var CybirdButton = document.getElementById("Cybird");
     CybirdButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://www.cybird.co.jp/';
     }
 
     var dorasuButton = document.getElementById("dorasu");
     dorasuButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'http://www.dorasu.com/';
     }
 
     var aquriaButton = document.getElementById("geechs");
     aquriaButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://geechs.com/';
     }
 
     var CybirdButton = document.getElementById("Granzella");
     CybirdButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'http://granzellagamefactory.co.jp/';
     }
 
     var dorasuButton = document.getElementById("Klab");
     dorasuButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://www.klab.com/jp/';
     }
 
     var aquriaButton = document.getElementById("Stylagy");
     aquriaButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://www.stylagy.co.jp/service/social_app.html';
     }
 
     var CybirdButton = document.getElementById("sun");
     CybirdButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'http://www.sun-denshi.co.jp/';
     }
 
     var dorasuButton = document.getElementById("ThreeRings");
     dorasuButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://www.threerings.jp/outline.html';
     }
 
     var dorasuButton = document.getElementById("YuKes");
     dorasuButton.onclick = function () {
-        window.location.href = '';
+        window.location.href = 'https://www.yukes.co.jp/';
     }
 
 
