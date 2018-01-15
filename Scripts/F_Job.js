555b9b49328b928c7d6343303b326c4a346d6427
Fujii <fkei120@yahoo.co.jp>
2018-01-15 09:53:17 +0900

    更新

---
+++ Scripts/F_Job.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+   
+    var cyberagentButton = document.getElementById("cyberagent");
+    cyberagentButton.onclick = function ()
+    {
+        window.location.href = 'https://www.cyberagent.co.jp/service/game/';
+    }
+
+    var wonderplanetButton = document.getElementById("wonderplanet");
+    wonderplanetButton.onclick = function ()
+    {
+        window.location.href = 'https://wonderpla.net/';
+    }
+
+    var coroplaButton = document.getElementById("coropla");
+    coroplaButton.onclick = function ()
+    {
+        window.location.href = 'http://colopl.co.jp/index.php';
+    }
+
+    var aquriaButton = document.getElementById("aquria");
+    aquriaButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var CybirdButton = document.getElementById("Cybird");
+    CybirdButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var dorasuButton = document.getElementById("dorasu");
+    dorasuButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var aquriaButton = document.getElementById("geechs");
+    aquriaButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var CybirdButton = document.getElementById("Granzella");
+    CybirdButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var dorasuButton = document.getElementById("Klab");
+    dorasuButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var aquriaButton = document.getElementById("Stylagy");
+    aquriaButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var CybirdButton = document.getElementById("sun");
+    CybirdButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var dorasuButton = document.getElementById("ThreeRings");
+    dorasuButton.onclick = function () {
+        window.location.href = '';
+    }
+
+    var dorasuButton = document.getElementById("YuKes");
+    dorasuButton.onclick = function () {
+        window.location.href = '';
+    }
+
+
+
+
+}, false);
