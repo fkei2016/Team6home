c57eac51e921e023c8778d9e46218e292d264e16
TSITCL\s152100 <sasamoto2016@gmail.com>
2018-01-29 10:50:03 +0900

    若干ごり押しで全部にモーション実装

--- Scripts/F_Job.js
+++ Scripts/F_Job.js
@@ -80,9 +80,100 @@ $(function () {
     //    $('#cyberagentStudent').fadeOut("slow");
     //});
     $('#cyberagent').mouseover(function () {
-        $("#cyberagentStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+        $(".cyberagentStudent").animate({ "opacity": "1", "marginLeft": "225px" });
     });
     $('#cyberagent').mouseout(function () {
-        $("#cyberagentStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+        $(".cyberagentStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+    $('#wonderplanet').mouseover(function () {
+        $(".wonderplanetStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#wonderplanet').mouseout(function () {
+        $(".wonderplanetStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+    $('#coropla').mouseover(function () {
+        $(".coroplaStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#coropla').mouseout(function () {
+        $(".coroplaStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#aquria').mouseover(function () {
+        $(".aquriaStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#aquria').mouseout(function () {
+        $(".aquriaStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#Cybird').mouseover(function () {
+        $(".CybirdStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#Cybird').mouseout(function () {
+        $(".CybirdStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#dorasu').mouseover(function () {
+        $(".dorasuStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#dorasu').mouseout(function () {
+        $(".dorasuStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#geechs').mouseover(function () {
+        $(".geechsStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#geechs').mouseout(function () {
+        $(".geechsStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#Granzella').mouseover(function () {
+        $(".GranzellaStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#Granzella').mouseout(function () {
+        $(".GranzellaStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#Klab').mouseover(function () {
+        $(".KlabStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#Klab').mouseout(function () {
+        $(".KlabStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+
+    $('#Stylagy').mouseover(function () {
+        $(".StylagyStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#Stylagy').mouseout(function () {
+        $(".StylagyStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+    $('#sun').mouseover(function () {
+        $(".sunStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#sun').mouseout(function () {
+        $(".sunStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+    $('#ThreeRings').mouseover(function () {
+        $(".ThreeRingsStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#ThreeRings').mouseout(function () {
+        $(".ThreeRingsStudent").animate({ "opacity": "0", "marginLeft": "0px" });
+    });
+
+    $('#YuKes').mouseover(function () {
+        $(".YuKesStudent").animate({ "opacity": "1", "marginLeft": "225px" });
+    });
+    $('#YuKes').mouseout(function () {
+        $(".YuKesStudent").animate({ "opacity": "0", "marginLeft": "0px" });
     });
 });
